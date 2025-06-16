7209389b76a3e2605f53bc470aabc27e6cd006cc
gm-ds <128259668+gm-ds@users.noreply.github.com>
2025-06-16 09:37:01 +0000

    Mv files

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-from src import cli, core, output, utils
--- src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.utils.mock_functions import mock_get_default_config_path
-from src.utils.example_config import example
-from src.utils.globals import *
