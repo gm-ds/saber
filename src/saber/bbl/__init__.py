f02d05670970686f627db349a0d6c726a2e0a7b5
gm-ds <128259668+gm-ds@users.noreply.github.com>
2025-06-16 17:08:10 +0000

    Logger like typing

--- src/saber/bbl/__init__.py
+++ src/saber/bbl/__init__.py
@@ -1,4 +1,5 @@
 from saber.bbl.logger import CustomLogger
 from saber.bbl.bioblend_testjobs import GalaxyTest
+from saber.bbl.loglike import LoggerLike
 
-__all__ = ["CustomLogger", "GalaxyTest"]
+__all__ = ["CustomLogger", "GalaxyTest", "LoggerLike"]
